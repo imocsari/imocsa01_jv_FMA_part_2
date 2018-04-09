e288fdd3a771c744358cc8ed75d58c795585658e
Imre Mocsari <imremocsari1@yahoo.co.uk>
2018-04-09 16:18:37 +0100

    fully working validation

--- form.js
+++ form.js
@@ -1,17 +1,19 @@
 function validateForm() {
     var regexFirstName = /^[a-z]+$/i;
     var firstName = document.getElementById("fname").value;
-    if (firstName === "" || (!regexFirstName.test(firstName))) {
+    if (firstName === "" || (!regexFirstName.test(firstName)) || firstName.length < 2) {
         document.getElementById("errfName").style.display="inline";
         return false;
     }
-    var regexLastName = /^[a-z -]+$/i;
+    var regexLastName = /^[a-z-]+$/i;
     var lastName = document.getElementById("lname").value;
-    if (lastName === "" || (!regexLastName.test(lastName))) {
-        document.getElementById("errfName").style.display="inline";
+    if (lastName === "" || (!regexLastName.test(lastName)) || lastName.length < 2) {
+        document.getElementById("errlName").style.display="inline";
         return false;
     }
-    if (document.getElementById("han").value === "") {
+    var regexHealthNumber = /^\d{6}$/;
+    var healthNumber = document.getElementById("han").value;
+    if (healthNumber === "" || healthNumber.length > 9 || (!regexHealthNumber.test(parseInt(healthNumber.substring(3)))) || (healthNumber.substring(0,3) !== "ZHA")) {
         document.getElementById("errHan").style.display="inline";
         return false;
     }
@@ -21,7 +23,9 @@ function validateForm() {
         document.getElementById("errEmail").style.display="inline";
         return false;
     }
-    if (document.getElementById("telephone").value === "") {
+    var regexPhone = /^\d{11}$/;
+    var phone = document.getElementById("telephone").value
+    if (phone === "" || !regexPhone.test(phone)) {
         document.getElementById("errPhone").style.display="inline";
         return false;
     }
